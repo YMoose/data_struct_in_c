9055c58cead382e180a9f0e26a7db89147660831
moose <1728093819@qq.com>
2024-08-04 14:36:14 +0000

    update lock

---
+++ src/lock.h
@@ -0,0 +1,23 @@
+#ifndef __INCLUDE_LOCK_H__
+#define __INCLUDE_LOCK_H__
+
+// mutex
+// init
+#ifdef __WINDOWS__
+#endif /* __WINDOWS__ */
+#ifdef __LINUX__
+#endif /* __LINUX__ */
+
+// lock
+#ifdef __WINDOWS__
+#endif /* __WINDOWS__ */
+#ifdef __LINUX__
+#endif /* __LINUX__ */
+
+// unlock
+#ifdef __WINDOWS__
+#endif /* __WINDOWS__ */
+#ifdef __LINUX__
+#endif /* __LINUX__ */
+
+#endif /* __INCLUDE_LOCK_H__ */
