e943a248b2a5429bdd373d3761ceefb0aff5808e
moose <1728093819@qq.com>
2024-08-05 14:34:53 +0000

    update lock

--- src/lock.h
+++ src/lock.h
@@ -1,23 +1,61 @@
 #ifndef __INCLUDE_LOCK_H__
 #define __INCLUDE_LOCK_H__
 
-// mutex
-// init
-#ifdef __WINDOWS__
-#endif /* __WINDOWS__ */
+#define __LINUX__
+
 #ifdef __LINUX__
-#endif /* __LINUX__ */
+#include <pthread.h>
+#elif __WINDOWS__
+#endif /* __LINUX__/__WINDOWS__ */
+
+
+// mutex
+typedef struct _mutex_t
+{
+#ifdef __LINUX__
+    pthread_mutex_t lock;
+#elif __WINDOWS__
+#endif /* __LINUX__/__WINDOWS__ */
+}mutex_t;
+
+// init
+static inline 
+int mutex_init(mutex_t* lock)
+{
+#ifdef __LINUX__
+    pthread_mutex_init(&(lock->lock), NULL);
+#elif __WINDOWS__
+#endif /* __LINUX__/__WINDOWS__ */
+}
+
+// destory
+static inline
+int mutex_destory(mutex_t* lock)
+{
+#ifdef __LINUX__
+    pthread_mutex_destory(&(lock->lock));
+#elif __WINDOWS__
+#endif /* __LINUX__/__WINDOWS__ */
+}
 
 // lock
-#ifdef __WINDOWS__
-#endif /* __WINDOWS__ */
+static inline
+int mutex_lock(mutex_t* lock)
+{
 #ifdef __LINUX__
-#endif /* __LINUX__ */
+    pthread_mutex_lock(&(lock->lock));
+#elif __WINDOWS__
+#endif /* __LINUX__/__WINDOWS__ */
+}
 
 // unlock
-#ifdef __WINDOWS__
-#endif /* __WINDOWS__ */
+static inline
+int mutex_unlock(mutex_t* lock)
+{
 #ifdef __LINUX__
-#endif /* __LINUX__ */
+    pthread_mutex_unlock(&(lock->lock));
+#elif __WINDOWS__
+#endif /* __LINUX__/__WINDOWS__ */
+}
 
 #endif /* __INCLUDE_LOCK_H__ */
